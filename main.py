fe86229af04a5e6cd0b48c02f4ecab81f0a6ef7e
amytee252 <amytee252@hotmail.com>
2022-05-19 12:29:47 +0100

    updates

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #### Script that contains solutions to Prospective test
 import numpy as np
+
 import pandas as pd
+from pandas import Series
+
 
 
 ### T1Q1. Calculate and plot the daily OTP for route 56 during March 2022. i.e. report_data vs. OTP percentage. Note the OTP is expected to vary day by day and be systematically different on weekends.
@@ -42,16 +45,49 @@ df_trip = {}
 
 days = df_TO_2['report_date'].nunique()
 
+#Convert all time data columns to seconds
+#SUPER CLUNKY CONVERT TO FUNCTION!!!!
+df_TO_2['scheduled_start_time'] = pd.to_datetime(df_TO_2.scheduled_start_time, format = '%H:%M:%S')
+df_TO_2['scheduled_start_time'] = df_TO_2['scheduled_start_time'].dt.hour * 3600 + df_TO_2['scheduled_start_time'].dt.minute * 60 + df_TO_2['scheduled_start_time'].dt.second
+
+
+df_TO_2['scheduled_arrival'] = pd.to_datetime(df_TO_2.scheduled_arrival, format = '%H:%M:%S')
+df_TO_2['scheduled_arrival'] = df_TO_2['scheduled_arrival'].dt.hour * 3600 + df_TO_2['scheduled_arrival'].dt.minute * 60 + df_TO_2['scheduled_arrival'].dt.second
+
+df_TO_2['scheduled_departure'] = pd.to_datetime(df_TO_2.scheduled_departure, format = '%H:%M:%S')
+df_TO_2['scheduled_departure'] = df_TO_2['scheduled_departure'].dt.hour * 3600 + df_TO_2['scheduled_departure'].dt.minute * 60 + df_TO_2['scheduled_departure'].dt.second
+
+df_TO_2['observed_arrival'] = pd.to_datetime(df_TO_2.observed_arrival, format = '%H:%M:%S')
+df_TO_2['observed_arrival'] = df_TO_2['observed_arrival'].dt.hour * 3600 + df_TO_2['observed_arrival'].dt.minute * 60 + df_TO_2['observed_arrival'].dt.second
+
+df_TO_2['observed_departure'] = pd.to_datetime(df_TO_2.observed_departure, format = '%H:%M:%S')
+df_TO_2['observed_departure'] = df_TO_2['observed_departure'].dt.hour * 3600 + df_TO_2['observed_departure'].dt.minute * 60 + df_TO_2['observed_departure'].dt.second
+
+print(df_TO_2)
+
 for day in range(days):
-	print(day)
-	df_day[day] = pd.DataFrame()
-	df_day[day] = grouped_day.get_group(day)
-	trips = df_day[day]['trip_id'].nunique()
-	grouped_trip = df_day[day].groupby(['trip_id'])
-	print(df_day[day])
+	#print(day)
+	df_day[day] = pd.DataFrame()  #Create an empty dataframe
+	df_day[day] = grouped_day.get_group(day) #Fill dataframe with only one day's rows of data, e.g day 1 only
+	trips = df_day[day]['trip_id'].unique() #Get the unique trip ids
+	trips_to_int = {trip: i for i, trip in enumerate(trips)} #convert each unique trip id to a number
+	df_day[day] = df_day[day].replace(trips_to_int) # replace each trip id with a number
+	#print(df_day[day])
+	grouped_trip = df_day[day].groupby(['trip_id']) #group rows of data by their trip_id
+	#print(grouped_trip)
+	trips = df_day[day]['trip_id'].nunique() # Get the number of unique trip ids
+	print('trips: ', trips)
+	on_time_counter = 0
+	late_time_counter = 0
+	#print(df_day[day])
 	for trip in range(trips):
-		df_trip[trip] = pd.DataFrame()
-		df_trip[trip] = grouped_trip.get_group(trip)
+		df_trip[trip] = pd.DataFrame() #Create an empty dataframe
+		df_trip[trip] = grouped_trip.get_group(trip) # Fill will information of a single trip_id.
+	
+		
+		
+		
+		
 		
 	
 
